3344b5d27b1fede6ff31ed5eeed49498baa098d9
vitalik-ctrl <vitalikvarhanik@gmail.com>
2023-12-25 19:37:11 +0100

    added simple filter and added related methods in TrainingCorpus

---
+++ filtercounter.py
@@ -0,0 +1,38 @@
+from trainingcorpus import TrainingCorpus
+from basefilter import BaseFilter
+
+
+class RelativeCountFilter(BaseFilter):
+    def __init__(self):
+        super().__init__()
+        self.spam_words = {}
+        self.spam_count = 0
+        self.ham_count = 0
+        self.ratio = 0
+
+    def predict(self, email):
+        words = email.split()
+        for word in words:
+            if self.spam_words.get(word, 0) > 37:
+                return True
+        return False
+
+    def train(self, corpus_dir):
+        corpus = TrainingCorpus(corpus_dir)
+        self.spam_count = corpus.count_spam()
+        self.ham_count = corpus.count_ham()
+        self.ratio = self.spam_count / (self.spam_count + self.ham_count)
+
+        for _, spam_content in corpus.spams():
+            for word in spam_content.split():
+                if word not in self.spam_words:
+                    self.spam_words[word] = 0
+                self.spam_words[word] += self.ratio
+
+        for _, ham_content in corpus.hams():
+            for word in ham_content.split():
+                if word in self.spam_words:
+                    self.spam_words[word] -= 1000 * self.ratio
+
+    def test(self, corpus_test_dir):
+        super().test(corpus_test_dir)
--- trainingcorpus.py
+++ trainingcorpus.py
@@ -7,13 +7,23 @@ class TrainingCorpus(Corpus):
     def __init__(self, path):
         super().__init__(path)
         self.classifications = read_classification_from_file(os.path.join(self.path, '!truth.txt'))
-        
+        self.spam_count = 0
+        self.ham_count = 0
+
+    def count_spam(self):
+        self.spam_count = len(list(filter(lambda x: x == 'SPAM', self.classifications.values())))
+        return self.spam_count
+
+    def count_ham(self):
+        self.ham_count = len(list(filter(lambda x: x == 'OK', self.classifications.values())))
+        return self.ham_count
+
     def spams(self):
         for email in self.emails():
             if self.classifications[email[0]] == 'SPAM':
-                yield email[1]
-                
+                yield email
+
     def hams(self):
         for email in self.emails():
             if self.classifications[email[0]] == 'OK':
-                yield email[1]
+                yield email
