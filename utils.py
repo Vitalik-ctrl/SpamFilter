3810ae40dd46b183b3a2ff17ad73b32657a9a428
vitalik-ctrl <vitalikvarhanik@gmail.com>
2023-12-25 16:57:02 +0100

    added write_dict_to_file() function

--- utils.py
+++ utils.py
@@ -5,4 +5,9 @@ def read_classification_from_file(filename):
             name_class = line.split()
             classifications[name_class[0]] = name_class[1]
         return classifications
-    
+
+
+def write_dict_to_file(file, dictionary):
+    with open(file, 'w', encoding='UTF-8') as file:
+        for key, value in dictionary.items():
+            file.write(f"{key} {value}\n")
